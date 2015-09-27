b46484293fad9bccb5a8b1c70f555148ba02fc90
Gian Marco Sibilla <gm.sibilla@gmail.com>
2015-09-27 19:01:45 +0200

    Fixed image tag

--- tinyOS/tinyos.sh
+++ tinyOS/tinyos.sh
@@ -7,4 +7,4 @@ fi
 
 echo "Mounting $1 to /code"
 echo "Run \"make tmote sim\" to build the app for Tossim simulator"
-docker run --privileged --rm -it --name tinyos_simple -v $1:/code tinyos /bin/bash
+docker run --privileged --rm -it --name tinyos_simple -v $1:/code gmbilla/tinyos /bin/bash
