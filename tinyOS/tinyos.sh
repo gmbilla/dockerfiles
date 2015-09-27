6baa46c8f59479ea3cedfac5abcd2a36ea5f988d
Gian Marco Sibilla <gm.sibilla@gmail.com>
2015-09-27 18:28:34 +0200

    Added helper script to run TinyOS project in container

---
+++ tinyOS/tinyos.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [[ $# < 1 ]]; then
+	echo "USAGE $0 /path/to/project_folder"
+	exit 1
+fi
+
+echo "Mounting $1 to /code"
+echo "Run \"make tmote sim\" to build the app for Tossim simulator"
+docker run --privileged --rm -it --name tinyos_simple -v $1:/code tinyos /bin/bash
